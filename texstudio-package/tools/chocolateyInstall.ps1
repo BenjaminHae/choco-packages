7919fa961cc0059bad0a7489306ed61f5251015f
Benjamin H盲ublein <BenjaminHae@users.noreply.github.com>
2014-11-19 18:59:26 +0100

    done with my first package

--- texstudio-package/tools/chocolateyInstall.ps1
+++ texstudio-package/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'TeXstudio'
 $installerType = 'EXE'
-$url = 'http://sourceforge.net/projects/texstudio/files/texstudio/TeXstudio%202.8.6/texstudio2.8.6_win_qt5.3.1.exe/download' # download url
-$silentArgs = '/VERYSILENT' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
+$url = 'http://sourceforge.net/projects/texstudio/files/texstudio/TeXstudio%202.8.6/texstudio2.8.6_win_qt5.3.1.exe/download'
+$silentArgs = '/VERYSILENT'
 $validExitCodes = @(0)
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
