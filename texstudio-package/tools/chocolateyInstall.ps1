3627d6579840c52be2832ee11d7a825269a1e360
Benjamin H盲ublein <BenjaminHae@users.noreply.github.com>
2015-03-31 19:54:58 +0200

    2.9.4

--- texstudio-package/tools/chocolateyInstall.ps1
+++ texstudio-package/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'TeXstudio'
 $installerType = 'EXE'
-$url = 'http://sourceforge.net/projects/texstudio/files/texstudio/TeXstudio%202.8.8/texstudio2.8.8_win_qt5.3.1.exe/download'
+$url = 'http://sourceforge.net/projects/texstudio/files/texstudio/TeXstudio%202.9.4/texstudio2.9.4_win_qt5.4.1.exe/download'
 $silentArgs = '/VERYSILENT'
 $validExitCodes = @(0)
 
